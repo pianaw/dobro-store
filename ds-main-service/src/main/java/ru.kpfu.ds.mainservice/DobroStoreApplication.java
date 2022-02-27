4566e0eebfec6a4ea9e78244220eeb6c8a0a1370
Diana.Monzhosova <Diana.Monzhosova@waveaccess.ru>
2022-02-27 20:25:58 +0300

    added spring boot initial config

---
+++ ds-main-service/src/main/java/ru.kpfu.ds.mainservice/DobroStoreApplication.java
@@ -0,0 +1,12 @@
+package ru.kpfu.ds.mainservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DobroStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DobroStoreApplication.class, args);
+    }
+}
