6a024fe341e001a2004503d00da0e7f8380e7d04
Diana.Monzhosova <Diana.Monzhosova@waveaccess.ru>
2022-05-22 13:25:33 +0300

    added cors configs

--- ds-main-service/src/main/java/ru/kpfu/ds/mainservice/config/CorsConfig.java
+++ ds-main-service/src/main/java/ru/kpfu/ds/mainservice/config/CorsConfig.java
@@ -1,5 +1,9 @@
 package ru.kpfu.ds.mainservice.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
